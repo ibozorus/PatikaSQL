0c00cbb692655b8f2fca1c4e634fe462d81ff380
ibozorus <korkmazie@gmail.com>
2023-03-14 09:58:56 +0100

    Ödev 1 yapildi

---
+++ odev1.sql
@@ -0,0 +1,11 @@
+SELECT title, description FROM film;
+
+SELECT * FROM film WHERE length < 75 AND length > 60;
+
+SELECT * FROM film WHERE (rental_rate = 0.99) AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
+
+SELECT last_name FROM customer WHERE first_name='Mary';
+
+SELECT * FROM film WHERE length <= 50 AND rental_rate != 2.99 AND rental_rate != 4.99;
+
+
